fdf134c754a8303dccb7566564d0f99706bae65c
Jeremy McCormick <jjmccormick@crimson.ua.edu>
2018-01-16 16:44:53 -0600

    Finished struct def

--- SingleLinkedList.h
+++ SingleLinkedList.h
@@ -2,9 +2,38 @@
 // SingleLinkedList.h
 // CS201 Assignment 0
 
+#ifndef SINGLELINKEDLIST_H
+#define SINGLELINKEDLIST_H
 struct NodeTag;
 
 typedef struct NodeTag
 {
-    Node *
+    Node * next;
+    void * data;
+    int (*cmp) (void * data1, void * data2) compare;
 } Node;
+
+Node * head;
+Node * current;
+Node * end;
+
+void * nextNode()
+{
+    current = current->next;
+    return current->data;
+}
+
+void addFront(void * data)
+{
+    Node newNode;
+    newNode->data = data;
+    newNode->next = head;
+    head = newNode;
+}
+
+void addEnd(void * data)
+{
+    Node newNode;
+}
+
+#endif
